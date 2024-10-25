e91850d5e7f60ab6d8a05f97c409346382957978
P3D\thomas.mol <tm.adm1718@gmail.com>
2024-10-25 11:35:10 +0200

    Ability SuperJump working

---
+++ Assets/Scripts/Abilities/AbilitySuperJump.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitySuperJump : Ability
+{
+    [SerializeField] float SuperJumpForce;
+    Vector3 Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Force = new Vector3(0, SuperJumpForce, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    override public void OnActionButtonPressed()
+    {
+        MainGame.Instance.player.GetPlayerRigidBody().AddForce(Force, ForceMode2D.Impulse);
+    }
+}
+
